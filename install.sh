167fd5424ffe6fb1f836e792eb97a784966aa07f
Ben O'Bryan <50787535+SolvedSphinx126@users.noreply.github.com>
2021-12-30 22:57:21 -0600

    add support for .zip file compression
    based on https://github.com/whiskerz007/proxmox_hassos_install/issues/123#issuecomment-985943330

--- install.sh
+++ install.sh
@@ -113,6 +113,7 @@ msg "Extracting disk image..."
 case $FILE in
   *"gz") gunzip -f $FILE;;
   *"xz") xz -d $FILE;;
+  *"zip") unzip $FILE;;
   *) die "Unable to handle file extension '${FILE##*.}'.";;
 esac
 
